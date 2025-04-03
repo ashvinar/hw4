239b7dbaaaa5e5dd1a6952e39efb18693f2410d9
Ashvin Arora <ashvinar@usc.edu>
2025-04-03 06:35:52 +0000

    First Push

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,33 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+std::pair<int, int> leafDepths(Node* node, int currentDepth);
 
 bool equalPaths(Node * root)
 {
     // Add your code below
 
+    if (root == nullptr) return true;
+
 }
 
+
+std::pair<int, int> leafDepths(Node* node, int currentDepth){
+
+  if (node->left == nullptr && node->right == nullptr){
+    return {currentDepth, currentDepth};
+  }
+
+  if (node->left != nullptr)
+    std::pair<int,int>leftPair = computeLeafDepths(node->left, currentDepth + 1);
+  
+  if (node->right != nullptr)
+    std::pair<int,int>rightPair = computeLeafDepths(node->right, currentDepth + 1);
+
+  if (node->right != nullptr && node->left != nullptr)
+    std::pair<int,int>leftPair = computeLeafDepths(node->left, currentDepth + 1);
+    std::pair<int,int>rightPair = computeLeafDepths(node->right, currentDepth + 1);
+
+    minDepth = min(leftPair)
+
+}
